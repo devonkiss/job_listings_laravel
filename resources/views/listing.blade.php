3b108e64d32b89a430869347d31c9e9670d389fd
Devon Kiss <tuffs@gmx.com>
2022-11-13 23:43:25 -0600

    Updates to Blade templating.

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,32 +1,64 @@
 @extends('layout')
 
 @section('content')
-<div class="small_description">
-  <h3>{{$listing['title']}}</h3>
-  <hr/>
-  <div>
-    <strong>Company: </strong>{{$listing['company']}}<br/>
-  </div>
-  <div>
-    <strong>Email: </strong>{{$listing['email']}}<br/>
-  </div>
-  <div>
-    <strong>Web URL: </strong>{{$listing['website']}}<br/>
-  </div>
-  <div>
-    <strong>Location: </strong>{{$listing['location']}}<br/>
-  </div>
-  <div>
-    <strong>Description: </strong>{{$listing['description']}}<br/>
-  </div>
-</div>
-<div style="width:100%;" class="margin-lg">
-  <center>
-    <strong>
-      <a href="/listings/all" class="button">
-        Back to All Listings
-      </a>
-    </strong>
-  </center>
-</div>
+@include('partials._search')
+  <main>
+    <a href="/" class="inline-block text-black ml-4 mb-4"
+        ><i class="fa-solid fa-arrow-left"></i> Back
+    </a>
+    <div class="mx-4">
+      <div class="bg-gray-50 border border-gray-200 p-10 rounded">
+        <div class="flex flex-col items-center justify-center text-center">
+          <img
+            class="w-48 mr-6 mb-6"
+            src="{{ asset('images/no-image.png') }}"
+            alt=""
+          />
+          <h3 class="text-2xl mb-2">{{$listing->title}}</h3>
+          <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
+          <ul class="flex">
+            <li class="bg-black text-white rounded-xl px-3 py-1 mr-2">
+              <a href="#">Laravel</a>
+            </li>
+            <li class="bg-black text-white rounded-xl px-3 py-1 mr-2">
+              <a href="#">API</a>
+            </li>
+            <li class="bg-black text-white rounded-xl px-3 py-1 mr-2">
+              <a href="#">Backend</a>
+            </li>
+            <li class="bg-black text-white rounded-xl px-3 py-1 mr-2">
+              <a href="#">Vue</a>
+            </li>
+          </ul>
+          <div class="text-lg my-4">
+            <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
+          </div>
+          <div class="border border-gray-200 w-full mb-6"></div>
+          <div>
+          <h3 class="text-3xl font-bold mb-4">
+            Job Description
+          </h3>
+          <div class="text-lg space-y-6">
+            <p>{{$listing->description}}</p>
+            <a
+              href="mailto:{{$listing->email}}"
+              class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80"
+              target="_blank"
+            >
+              <i class="fa-solid fa-envelope"></i>
+              Contact Employer
+            </a>
+            <a
+              href="{{$listing->website}}"
+              target="_blank"
+              class="block bg-black text-white py-2 rounded-xl hover:opacity-80"
+            >
+              <i class="fa-solid fa-globe"></i> Visit Website
+            </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 @endsection
