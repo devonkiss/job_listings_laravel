8118f6f60d6263f25ab0f9c66f479ae820590fa4
Devon Kiss <tuffs@gmx.com>
2022-11-15 02:10:22 -0600

    Created a footer component and added to the layout.blae.php component to truncate and seperate concerns.

--- resources/views/partials/_search.blade.php
+++ resources/views/partials/_search.blade.php
@@ -7,7 +7,7 @@
       type="text"
       name="search"
       class="h-14 w-full pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
-      placeholder="Search Laragigs..."
+      placeholder="Search Laragigs Listings..."
     />
     <div class="absolute top-2 right-2">
       <button type="submit" class="h-10 w-20 text-0 rounded-lg bg-red-500 hover:bg-red-600">
